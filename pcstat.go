35529b1a27131fe089114ddd0570d346e1ab7640
Al Tobey <tobert@gmail.com>
2014-05-01 02:44:50 -0700

    Use a proper Apache 2.0 license header.

--- pcstat.go
+++ pcstat.go
@@ -1,11 +1,26 @@
 package main
 
-// pcstat.go - page cache stat
-// uses the mincore(2) syscall to find out which pages (almost always 4k)
-// of a file are currently cached in memory
-//
-// Copyright 2014 Albert P. Tobey <atobey@datastax.com> @AlTobey
-// License: Apache 2.0
+/*
+ * Copyright 2014 Albert P. Tobey <atobey@datastax.com> @AlTobey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * pcstat.go - page cache stat
+ *
+ * uses the mincore(2) syscall to find out which pages (almost always 4k)
+ * of a file are currently cached in memory
+ *
+ */
 
 import (
 	"encoding/json"
