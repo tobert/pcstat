8e712b779b8c33bdcfca6a3be516c683edd6c15e
Al Tobey <tobert@gmail.com>
2014-04-30 17:52:17 -0700

    Check in first pass.

---
+++ README.md
@@ -0,0 +1,40 @@
+# pcstat - get page cache statistics for files
+
+A common question when tuning databases and other IO-intensive applications is,
+"is Linux caching my data or not?" pcstat gets that information for you using
+the mincore(2) syscall.
+
+The fincore application from [linux-ftools](https://code.google.com/p/linux-ftools/) does the
+same thing and I read its source code. I chose not to use it because it appears to be abandoned
+and has some build problems.
+
+## Usage
+
+    pcstat filename
+    pcstat -json filename > data.json
+
+## Building
+
+    git clone https://github.com/tobert/pcstat.git
+    cd pcstat
+    go build
+    sudo cp -a pcstat /usr/local/bin
+    pcstat /usr/local/bin/pcstat
+
+## Requirements
+
+Any Go 1.x or higher should work fine. No external libraries are required.
+
+From the mincore(2) man page:
+
+* Available since Linux 2.3.99pre1 and glibc 2.2.
+* mincore() is not specified in POSIX.1-2001, and it is not available on all UNIX implementations.
+* Before kernel 2.6.21, mincore() did not return correct information some mappings.
+
+## Author
+
+Al Tobey <tobert@gmail.com> @AlTobey
+
+## License
+
+Apache 2.0
---
+++ pcstat.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+// pcStat: page cache status
+// Info: see os.FileInfo
+// Pages: array of booleans: true if cached, false otherwise
+type pcStat struct {
+	Info     os.FileInfo
+	Pages    int
+	Cached   int
+	Uncached int
+	Status   []bool
+}
+
+func main() {
+	flag.Parse()
+	for _, fname := range flag.Args() {
+		pcs := getMincore(fname)
+		percent := (pcs.Cached / pcs.Pages) * 100
+		log.Printf("%s: Size: %d bytes, Pages Cached %d, Uncached: %d, %d%% cached\n",
+			fname, pcs.Info.Size(), pcs.Cached, pcs.Uncached, percent)
+	}
+}
+
+func getMincore(fname string) pcStat {
+	f, err := os.Open(fname)
+	if err != nil {
+		log.Fatalf("Could not open file '%s' for read: %s\n", fname, err)
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		log.Fatalf("Could not stat file %s: %s\n", fname, err)
+	}
+	if fi.Size() == 0 {
+		log.Fatalf("%s appears to be 0 bytes in length\n", fname)
+	}
+
+	mmap, err := syscall.Mmap(int(f.Fd()), 0, int(fi.Size()), syscall.PROT_NONE, syscall.MAP_SHARED)
+	if err != nil {
+		log.Fatalf("Could not mmap file '%s': %s\n", fname, err)
+	}
+	// TODO: check for MAP_FAILED which is ((void *) -1)
+	// but maybe unnecessary since it looks like errno is always set when MAP_FAILED
+
+	// one byte per page, only LSB is used, remainder is reserved and clear
+	vecsz := (fi.Size() + int64(os.Getpagesize()) - 1) / int64(os.Getpagesize())
+	vec := make([]byte, vecsz)
+
+	mmap_ptr := uintptr(unsafe.Pointer(&mmap[0]))
+	size_ptr := uintptr(fi.Size())
+	vec_ptr := uintptr(unsafe.Pointer(&vec[0]))
+	ret, _, err := syscall.RawSyscall(syscall.SYS_MINCORE, mmap_ptr, size_ptr, vec_ptr)
+	if ret != 0 {
+		log.Fatalf("syscall SYS_MINCORE failed: %s", err)
+	}
+	defer syscall.Munmap(mmap)
+
+	pcs := pcStat{fi, int(vecsz), 0, 0, make([]bool, vecsz)}
+
+	// expose no bitshift only bool
+	for i, b := range vec {
+		if b%2 == 1 {
+			pcs.Status[i] = true
+			pcs.Cached++
+		} else {
+			pcs.Status[i] = false
+			pcs.Uncached++
+		}
+	}
+
+	return pcs
+}
