af10859ef9104fb03a62e6d047f449104b79e8e3
Al Tobey <tobert@gmail.com>
2015-02-10 17:46:29 -0800

    update copyright year

--- pcstat/main.go
+++ pcstat/main.go
@@ -1,7 +1,7 @@
 package main
 
 /*
- * Copyright 2014 Albert P. Tobey <atobey@datastax.com> @AlTobey
+ * Copyright 2014-2015 Albert P. Tobey <atobey@datastax.com> @AlTobey
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
