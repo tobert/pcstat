c8b3b94c2bc8825d5749c5d364a01ec342b5e777
Al Tobey <tobert@gmail.com>
2015-02-10 16:54:32 -0800

    move mmap/mincore code to its own func+file

---
+++ mincore.go
@@ -0,0 +1,71 @@
+package pcstat
+
+/*
+ * Copyright 2014 Albert P. Tobey <atobey@datastax.com> @AlTobey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+// mmap the given file, get the mincore vector, then
+// return it as an []bool
+func FileMincore(f *os.File, size int64) ([]bool, error) {
+	// mmap is a []byte
+	mmap, err := syscall.Mmap(int(f.Fd()), 0, int(size), syscall.PROT_NONE, syscall.MAP_SHARED)
+	if err != nil {
+		return nil, fmt.Errorf("could not mmap: %v", err)
+	}
+	// TODO: check for MAP_FAILED which is ((void *) -1)
+	// but maybe unnecessary since it looks like errno is always set when MAP_FAILED
+
+	// one byte per page, only LSB is used, remainder is reserved and clear
+	vecsz := (size + int64(os.Getpagesize()) - 1) / int64(os.Getpagesize())
+	vec := make([]byte, vecsz)
+
+	// get all of the arguments to the mincore syscall converted to uintptr
+	mmap_ptr := uintptr(unsafe.Pointer(&mmap[0]))
+	size_ptr := uintptr(size)
+	vec_ptr := uintptr(unsafe.Pointer(&vec[0]))
+
+	// use Go's ASM to submit directly to the kernel, no C wrapper needed
+	// mincore(2): int mincore(void *addr, size_t length, unsigned char *vec);
+	// 0 on success, takes the pointer to the mmap, a size, which is the
+	// size that came from f.Stat(), and the vector, which is a pointer
+	// to the memory behind an []byte
+	// this writes a snapshot of the data into vec which a list of 8-bit flags
+	// with the LSB set if the page in that position is currently in VFS cache
+	ret, _, err := syscall.Syscall(syscall.SYS_MINCORE, mmap_ptr, size_ptr, vec_ptr)
+	if ret != 0 {
+		return nil, fmt.Errorf("syscall SYS_MINCORE failed: %v", err)
+	}
+	defer syscall.Munmap(mmap)
+
+	mc := make([]bool, vecsz)
+
+	// there is no bitshift only bool
+	for i, b := range vec {
+		if b%2 == 1 {
+			mc[i] = true
+		} else {
+			mc[i] = false
+		}
+	}
+
+	return mc, nil
+}
