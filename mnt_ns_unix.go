03a06be530e9d2bbe0f673d944b4241955f599e9
Al Tobey <atobey@netflix.com>
2017-05-26 14:43:02 -0700

    update copyrights for year & email address

--- mnt_ns_unix.go
+++ mnt_ns_unix.go
@@ -3,7 +3,7 @@
 package pcstat
 
 /*
- * Copyright 2014 Albert P. Tobey <atobey@datastax.com> @AlTobey
+ * Copyright 2014-2017 A. Tobey <tobert@gmail.com> @AlTobey
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
